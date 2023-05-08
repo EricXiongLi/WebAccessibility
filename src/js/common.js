b64786a747bfc4b543e8720e8312bf564d18ed99
Xi Cheng <xi.cheng@uzh.ch>
2023-05-08 06:21:35 +0200

    Completed Exercise1-2, E3:(1)-(4), E4:(1), E5:(1)

--- src/js/common.js
+++ src/js/common.js
@@ -48,3 +48,68 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.navbar-toggler')
         .addEventListener('click', toggleNavigation, false);
 }, false);
+
+
+// xc_E2_Accessible font size: scale all the font sizes dynamically
+const root = document.documentElement;
+const computedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
+let fontSize = parseFloat(computedFontSize);
+const maxFontSize = 30;
+const minFontSize = 10;
+
+function handleFontSizeChange(change) {
+  fontSize += change;
+  if (fontSize > maxFontSize || fontSize < minFontSize) {
+    fontSize -= change;
+    return;
+  }
+  root.style.fontSize = `${fontSize}px`;
+}
+
+document.getElementById('font-increase-button').addEventListener('click', () => {
+  handleFontSizeChange(2);
+});
+
+document.getElementById('font-decrease-button').addEventListener('click', () => {
+  handleFontSizeChange(-2);
+});
+
+document.getElementById('font-increase-button-1').addEventListener('click', () => {
+  handleFontSizeChange(2);
+});
+
+document.getElementById('font-decrease-button-1').addEventListener('click', () => {
+  handleFontSizeChange(-2);
+});
+
+
+// xc_E3_Drop-down menu: updates the aria-expanded attribute of the menu button and Toggle navigation button to announce "expanded" or "collapsed" as appropriate
+// define the array of menu objects
+var menus = [
+  {
+    menuItem: document.getElementById('nav-bar-faculties'),
+    expanded: false
+  },
+  {
+    menuItem: document.getElementById('nav-bar-education'),
+    expanded: false
+  },
+  {
+    menuItem: document.getElementById('nav-bar-industry'),
+    expanded: false
+  },
+  {
+    menuItem: document.querySelector('.navbar-toggler'),
+    expanded: false
+  }
+];
+// traverse the array of menu objects
+menus.forEach(function(menu) {
+  menu.menuItem.addEventListener('click', function() {
+    menu.expanded = !menu.expanded;
+    menu.menuItem.setAttribute('aria-expanded', menu.expanded);
+  });
+});
+
+
+
